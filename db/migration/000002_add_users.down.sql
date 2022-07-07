b13b54f7634ef989c5b226718c1002ff80c81b72
aswad98 <bashiraswad@gmail.com>
2022-07-07 13:41:27 +0500

    remove some error

--- db/migration/000002_add_users.down.sql
+++ db/migration/000002_add_users.down.sql
@@ -1,5 +1,6 @@
+
 ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "owner_currency_key";
 
 ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "accounts_owner_fkey";
 
-ALTER TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "users";
